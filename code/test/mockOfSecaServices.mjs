40ff1e33459bd2fbacc16be9400fb4923aa73f60
49462 - Francisco Saraiva <a49462@alunos.isel.pt>
2023-12-01 00:18:24 +0000

    tests for the finished events and for the group almost done (new events in group, put update group and group details done)

--- code/test/mockOfSecaServices.mjs
+++ code/test/mockOfSecaServices.mjs
@@ -1,16 +1,21 @@
 import errors from "../errors.mjs";
 import { readFile } from "node:fs/promises";
-import path from "node:path";
 
-export default function (secaTmMock) {
+export default function (secaTmMock, secaGroupsData, secaUsersData) {
   if (!secaTmMock) {
     throw errors.INVALID_PARAMETER("SECA DATA");
   }
-
+  if (!secaGroupsData) {
+    throw errors.INVALID_PARAMETER("GROUPS DATA");
+  }
+  if (!secaUsersData) {
+    throw errors.INVALID_PARAMETER("USERS DATA");
+  }
   return {
     getPopularEvents: getPopularEvents,
     getSearchedEvents: getSearchedEvents,
     getEventById: getEventById,
+    addEventToGroup: addEventToGroup,
   };
 
   async function getPopularEvents() {
@@ -34,6 +39,15 @@ export default function (secaTmMock) {
     throw errors.EVENT_NOT_FOUND(`events`);
   }
 
+  async function addEventToGroup(groupId, idEvent, userToken) {
+    const userId = await secaUsersData.getUserId(userToken);
+    const group = await getGroup(groupId, userId);
+    const event = await getEventById();
+    if (group.events.findIndex((i) => i.id == idEvent) != -1)
+      throw errors.EVENT_EXISTS(idEvent);
+    return await secaGroupsData.addEventToGroup(groupId, event);
+  }
+
   async function processFile(fileContent) {
     return JSON.parse(fileContent);
   }
@@ -47,4 +61,18 @@ export default function (secaTmMock) {
       console.log(error);
     }
   }
+
+   // Auxilry Functions
+   async function getGroup(groupId, userId) {
+    if (isNaN(Number(groupId))) {
+      throw errors.INVALID_ARGUMENT("groupId");
+    }
+    const group = await secaGroupsData.getGroup(groupId);
+    if (group.userId == userId) {
+      return group;
+    }
+    throw errors.NOT_AUTHORIZED();
+  }
+
+
 }
--- code/test/services.test.mjs
+++ code/test/services.test.mjs
@@ -12,13 +12,24 @@ import groupsService from "../services/seca-groups-services.mjs";
 import crypto from "crypto";
 import { describe } from "node:test";
 import secaEventsServices from "../services/seca-events-services.mjs";
+import { constants } from "buffer";
 
 // Test : npx mocha ./code/test/services-test.mjs
 
-const secaServices = mockEventsService(secaTmData);
-
+const secaServices = mockEventsService(
+  secaTmData,
+  secaGroupsData,
+  secaUsersData
+);
+const secaGroupServices = groupsService(
+  secaTmData,
+  secaGroupsData,
+  secaUsersData
+);
+//const usersService = usersService(secaUsersData);
 
 describe("SECA services", function () {
+
   describe("SECA Tests Events", function () {
     it("should return an object that is not undefined", async function () {
       //Arrange
@@ -52,7 +63,7 @@ describe("SECA services", function () {
       // Act
       // Assert
       assert.deepEqual(obj[0].id, "G5v0Z9Yc3BZyy");
-      assert.deepEqual(obj1[0].id, "Z7r9jZ1AdJ9uK");
+      assert.deepEqual(obj1[0].id, "vvG1zZ9YJwb39L");
       assert.deepEqual(obj2[0].id, "vvG1HZ9gn1xYD3");
     });
 
@@ -64,10 +75,8 @@ describe("SECA services", function () {
       // Act
       // Assert
       assert.deepEqual(obj[0].name, "Phoenix Suns vs. Memphis Grizzlies");
-      assert.deepEqual(obj1[0].name, "New Orleans Pelicans vs. Phoenix Suns");
-      assert.deepEqual(
-        obj2[0].name,
-        "Oregon Ducks Men's Basketball vs. UCLA Bruins Men's Basketball"
+      assert.deepEqual(obj1[0].name, "Atlanta Hawks vs. Phoenix Suns");
+      assert.deepEqual(obj2[0].name,"Oregon Ducks Men's Basketball vs. UCLA Bruins Men's Basketball"
       );
     });
 
@@ -79,7 +88,7 @@ describe("SECA services", function () {
       // Act
       // Assert
       assert.deepEqual(obj[0].date, "2023-12-02");
-      assert.deepEqual(obj1[0].date, "2024-01-19");
+      assert.deepEqual(obj1[0].date, "2024-02-02");
       assert.deepEqual(obj2[0].date, "2023-12-30");
     });
 
@@ -91,7 +100,7 @@ describe("SECA services", function () {
       // Act
       // Assert
       assert.deepEqual(obj[0].time, "19:00:00");
-      assert.deepEqual(obj1[0].time, "19:00:00");
+      assert.deepEqual(obj1[0].time, "19:30:00");
       assert.deepEqual(obj2[0].time, "13:00:00");
     });
 
@@ -132,7 +141,7 @@ describe("SECA services", function () {
       );
       assert.deepEqual(
         obj1[0].url,
-        "https://www.ticketmaster.com/event/Z7r9jZ1AdJ9uK"
+        "https://www.ticketmaster.com/atlanta-hawks-vs-phoenix-suns-atlanta-georgia-02-02-2024/event/0E005F09B26125DF"
       );
       assert.deepEqual(
         obj2[0].url,
@@ -140,6 +149,258 @@ describe("SECA services", function () {
       );
     });
   });
+
+  describe("SECA Tests Groups", function () {
+    describe("SECA Tests Groups - getGroupsDetails", function () {
+      it("should return the detailed groups", async function () {
+        //Arrange
+        const groupId = 4;
+        const userId = "14d72b99-48f6-48d3-94d3-5a4dcfd96c80";
+        const expectedGroup = {
+          description: 'Group 4 description',
+          events: [
+            {
+              date: '2024-01-19',
+              genre: 'Basketball',
+              id: 'Z7r9jZ1AdJ9uK',
+              name: 'New Orleans Pelicans vs. Phoenix Suns',
+              segment: 'Sports',
+              time: '19:00:00',
+              url: 'https://www.ticketmaster.com/event/Z7r9jZ1AdJ9uK'
+            }
+          ],
+          id: 4,
+          name: 'Group 4',
+          userId: 0
+        }
+        // Act
+        const group = await secaGroupServices.getGroupsDetails(groupId, userId);
+        // Assert
+       assert.deepEqual(group,expectedGroup);
+      });
+      it("should return an exception indicating that the user was not found", async function () {
+        //Arrange
+        const groupId = 4;
+        const userId = "14d72b99-48f6sdfdsf";
+        const expectedError = {
+          code: 2,
+          description: 'User Not Found'
+        };
+        // Act
+        try {
+          await secaGroupServices.getGroupsDetails(groupId, userId);
+        } catch (error) {
+         // Assert
+          assert.deepEqual(error, expectedError);
+          assert(error.code === expectedError.code);
+          assert(error.description === expectedError.description);
+        }
+      });
+      it("should return an exception indicating that the group was not found", async function () {
+        //Arrange
+        const groupId = 20;
+        const userId = "14d72b99-48f6-48d3-94d3-5a4dcfd96c80";
+        const expectedError = {
+          code: 3,
+          description: 'Group 20 not found'
+        };
+        // Act
+        try {
+          await secaGroupServices.getGroupsDetails(groupId, userId);
+        } catch (error) {
+         // Assert
+          assert.deepEqual(error, expectedError);
+          assert(error.code === expectedError.code);
+          assert(error.description === expectedError.description);
+        }
+      });
+    });
+
+    describe("SECA Tests Groups - updateGroup", async function () {
+      it("should return the updated group", async function () {
+        //Arrange
+        const groupId = 4;
+        const userId = "14d72b99-48f6-48d3-94d3-5a4dcfd96c80";
+        const name = "Francisco & Carolina"; 
+        const description = "SECA API 2324 IPW";
+        const expectedGroup = {
+          "id": 4,
+          "name": "Francisco & Carolina",
+          "description": "SECA API 2324 IPW",
+          "userId": 0,
+          "events": [
+              {
+                  "id": "Z7r9jZ1AdJ9uK",
+                  "name": "New Orleans Pelicans vs. Phoenix Suns",
+                  "date": "2024-01-19",
+                  "time": "19:00:00",
+                  "segment": "Sports",
+                  "genre": "Basketball",
+                  "url": "https://www.ticketmaster.com/event/Z7r9jZ1AdJ9uK"
+              }
+          ]
+      }
+        // Act
+        const group = await secaGroupServices.updateGroup(groupId, name, description, userId);
+        // Assert
+       assert.deepEqual(group,expectedGroup);
+      });
+
+      it("should return an exception indicating that the user was not found", async function () {
+        //Arrange
+        const groupId = 4;
+        const userId = "14d72b99-48f6sdfdsf";
+        const name = "Francisco & Carolina"; 
+        const description = "SECA API 2324 IPW";
+        const expectedError = {
+          code: 2,
+          description: 'User Not Found'
+        };
+        // Act
+        try {
+          await secaGroupServices.updateGroup(groupId, name, description, userId);
+        } catch (error) {
+         // Assert
+          assert.deepEqual(error, expectedError);
+          assert(error.code === expectedError.code);
+          assert(error.description === expectedError.description);
+        }
+      });
+
+      it("should return an exception indicating that the group was not found", async function () {
+        //Arrange
+        const groupId = 20;
+        const userId = "14d72b99-48f6-48d3-94d3-5a4dcfd96c80";
+        const name = "Francisco & Carolina"; 
+        const description = "SECA API 2324 IPW";
+        const expectedError = {
+          code: 3,
+          description: 'Group 20 not found'
+        };
+        // Act
+        try {
+          await secaGroupServices.updateGroup(groupId, name, description, userId);
+        } catch (error) {
+         // Assert
+          assert.deepEqual(error, expectedError);
+          assert(error.code === expectedError.code);
+          assert(error.description === expectedError.description);
+        }
+      });
+
+    });
+
+    describe("SECA Tests Groups - addEventToGroup", async function () {
+      it("sohuld return added event to group", async function () {
+      //  Arrange
+        const groupId = 4;
+        const idEvent = "vvG1zZ9YJwb39L";
+        const userToken = "14d72b99-48f6-48d3-94d3-5a4dcfd96c80";
+        const newEvent = {
+          "id": "vvG1zZ9YJwb39L",
+          "name": "Atlanta Hawks vs. Phoenix Suns",
+          "date": "2024-02-02",
+          "time": "19:30:00",
+          "segment": "Sports",
+          "genre": "Basketball",
+          "url": "https://www.ticketmaster.com/atlanta-hawks-vs-phoenix-suns-atlanta-georgia-02-02-2024/event/0E005F09B26125DF"
+      }
+
+      // Act
+        const group = await secaServices.addEventToGroup(groupId, idEvent, userToken);
+      // Assert
+      assert(group, newEvent);
+      });
+      
+      it("should return an exception indicating that the user was not found", async function () {
+      //  Arrange
+      const groupId = 4;
+      const idEvent = "vvG1zZ9YJwb39L";
+      const userToken = "14d72b99-48f6sdfdsf";
+      const expectedError = {
+        code: 2,
+        description: 'User Not Found'
+      };
+      // Act
+      try {
+        await secaServices.addEventToGroup(groupId, idEvent, userToken);
+      } catch (error) {
+       // Assert
+        assert.deepEqual(error, expectedError);
+        assert(error.code === expectedError.code);
+        assert(error.description === expectedError.description);
+      }
+
+
+      });
+      it("should return an exception indicating that the group was not found", async function () {
+        //  Arrange
+        const groupId = 20;
+        const idEvent = "vvG1zZ9YJwb39L";
+        const userToken = "14d72b99-48f6-48d3-94d3-5a4dcfd96c80";
+        const expectedError = {
+          code: 3,
+          description: 'Group 20 not found'
+        };
+        // Act
+        try {
+          await secaServices.addEventToGroup(groupId, idEvent, userToken);
+        } catch (error) {
+         // Assert
+          assert.deepEqual(error, expectedError);
+          assert(error.code === expectedError.code);
+          assert(error.description === expectedError.description);
+        }
+      });
+
+      it("should return an exception indicating that the event was not found", async function () {
+        //  Arrange
+        const groupId = 4;
+        const idEvent = "vvG1zZ9YJdsfdsfwb39L";
+        const userToken = "14d72b99-48f6-48d3-94d3-5a4dcfd96c80";
+        const expectedError = {
+          code: 4,
+          description: 'Event vvG1zZ9YJdsfdsfwb39L not found'
+        };
+        // Act
+        try {
+          await secaServices.addEventToGroup(groupId, idEvent, userToken);
+        } catch (error) {
+         // Assert
+          assert.deepEqual(error, expectedError);
+          assert(error.code === expectedError.code);
+          assert(error.description === expectedError.description);
+        }
+      });
+
+      it("should return an exception indicating that the event already exists", async function () {
+        //  Arrange
+        const groupId = 4;
+        const idEvent = "Z7r9jZ1AdJ9uK";
+        const userToken = "14d72b99-48f6-48d3-94d3-5a4dcfd96c80";
+        const expectedError = {
+          code: 5,
+          description: 'Event with id Z7r9jZ1AdJ9uK already exists'
+        };
+        // Act
+        try {
+          await secaServices.addEventToGroup(groupId, idEvent, userToken);
+        } catch (error) {
+         // Assert
+          assert.deepEqual(error, expectedError);
+          assert(error.code === expectedError.code);
+          assert(error.description === expectedError.description);
+        }
+      });
+
+    });
+
+
+
+  });
+
+
+
 });
 
 // Helper function
