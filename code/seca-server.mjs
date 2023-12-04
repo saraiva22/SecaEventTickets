6ce9bdbf813fc6134af4f24bdfba76cf466f2a4c
carolinatvs <a49465@alunos.isel.pt>
2023-12-04 23:18:20 +0000

    Popular Events Web Page

--- code/seca-server.mjs
+++ code/seca-server.mjs
@@ -1,70 +1,95 @@
+import swaggerUi from "swagger-ui-express";
+import yaml from "yamljs";
+
 import cors from "cors";
 import express from "express";
+import url from "url";
+import path from "path";
+import hbs from "hbs";
 
 // DATA IMPORTS
 import * as secaTmData from "./data/tm-events-data.mjs";
 import * as secaGroupsData from "./data/local/seca-groups-data-mem.mjs";
 import * as secaUsersData from "./data/local/seca-users-data-mem.mjs";
 // SERVICE IMPORTS
-import eventsService from "./services/seca-events-services.mjs";
-import usersService from "./services/seca-users-services.mjs";
-import groupsService from "./services/seca-groups-services.mjs";
+import eventsServiceInit from "./services/seca-events-services.mjs";
+import usersServiceInit from "./services/seca-users-services.mjs";
+import groupsServiceInit from "./services/seca-groups-services.mjs";
 // WEB API IMPORTS
-import eventsApi from "./web/api/seca-events-web-api.mjs";
-import usersApi from "./web/api/seca-users-web-api.mjs";
-import groupsApi from "./web/api/seca-groups-web-api.mjs";
-// SITE IMPORTS
-import * as staticWebSite from "./web/site/static-web-site.mjs";
+import eventsApiInit from "./web/api/seca-events-web-api.mjs";
+import usersApiInit from "./web/api/seca-users-web-api.mjs";
+import groupsApiInit from "./web/api/seca-groups-web-api.mjs";
+// WEB SITE IMPORTS
+import * as staticWebSite from "./web/site/seca-static-web-site.mjs";
+import eventsSiteInit from "./web/site/seca-events-web-site.mjs";
 
 // Events - Service and Web Api
-const secaEventsServices = eventsService(secaTmData);
-const eventsWebApi = eventsApi(secaEventsServices);
+const secaEventsServices = eventsServiceInit(secaTmData);
+const eventsWebApi = eventsApiInit(secaEventsServices);
+const eventsWebSite = eventsSiteInit(secaEventsServices);
 
 // Groups - Service and Web Api
-const secaGroupsServices = groupsService(
+const secaGroupsServices = groupsServiceInit(
   secaEventsServices,
   secaGroupsData,
   secaUsersData
 );
-const groupsWebApi = groupsApi(secaGroupsServices);
+const groupsWebApi = groupsApiInit(secaGroupsServices);
 
 // Users - Service and Web Api
-const secaUsersServices = usersService(secaUsersData);
-const usersWebApi = usersApi(secaUsersServices);
-
-// Web Site Imports
+const secaUsersServices = usersServiceInit(secaUsersData);
+const usersWebApi = usersApiInit(secaUsersServices);
 
 const PORT = 8080;
+const swaggerDocument = yaml.load("./docs/events-api.yaml");
 
 console.log("Setting up server");
 let app = express();
 
-//app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded());
 app.use("/site", express.static("./web/site/public"));
+
+// Handlebars view engine setup
+const currentFileDir = url.fileURLToPath(new URL('.', import.meta.url));
+const viewsDir = path.join(currentFileDir, 'web', 'site', 'views')
+app.set('view engine', 'hbs')
+app.set('views', viewsDir);
 
 // Web site routes
 app.get("/site/home", staticWebSite.getHome);
 
-app.get("/events/popular", eventsWebApi.getPopularEvents);
+// Web Api routes
+// Get Popular Events
+app.get("/events/popular", eventsWebSite.getPopularEvents);
 
+// Get Search Events according to keyword
 app.get("/events", eventsWebApi.getSearchedEvents);
 
+// Create User
 app.post("/users", usersWebApi.createUser);
 
+// Get All Groups of User
 app.get("/groups", groupsWebApi.getAllGroups);
 
+// Create New Group
 app.post("/groups", groupsWebApi.createGroup);
 
+// Get Group Details
 app.get("/groups/:groupId", groupsWebApi.getGroupsDetails);
 
+// Update Name and Group Description
 app.put("/groups/:groupId", groupsWebApi.updateGroup);
 
+// Delete a Group
 app.delete("/groups/:groupId", groupsWebApi.deleteGroup);
 
+// Add Event to One Group
 app.post("/groups/:groupId/events", groupsWebApi.addEventToGroup);
 
+// Delete an Event From Group
 app.delete(
   "/groups/:groupId/events/:eventsId",
   groupsWebApi.deleteEventFromGroup
---
+++ code/web/site/seca-events-web-site.mjs
@@ -0,0 +1,46 @@
+import { json } from "stream/consumers";
+import errors from "../../../common/errors.mjs";
+import errorToHttp from "../errors-to-http-responses.mjs";
+
+const SIZE = 30;
+const PAGE = 1;
+
+export default function (secaServices) {
+  if (!secaServices) {
+    throw errors.INVALID_PARAMETER("SECA SERVICES");
+  }
+
+  return {
+    getPopularEvents: processRequest(getPopularEvents),
+    getSearchedEvents: processRequest(getSearchedEvents),
+  };
+
+  function processRequest(reqProcessor) {
+    return async function (req, rsp) {
+      try {
+        return await reqProcessor(req, rsp);
+      } catch (e) {
+        const rspError = errorToHttp(e);
+        rsp.status(rspError.status).json(rspError.body);
+      }
+    };
+  }
+
+  async function getPopularEvents(req, rsp) {
+    const s = req.query.s != undefined ? req.query.s : SIZE;
+    const p = req.query.p != undefined ? req.query.p : PAGE;
+    const popular = await secaServices.getPopularEvents(s, p);
+    rsp.render('popularEvents', { events: popular });
+  }
+
+  async function getSearchedEvents(req, rsp) {
+    const keyword = req.query.keyword;
+    const s = req.query.s != undefined ? req.query.s : SIZE;
+    const p = req.query.p != undefined ? req.query.p : PAGE;
+    const events = await secaServices.getSearchedEvents(keyword, s, p);
+    rsp.status(200).json({
+      status: "Sucess - searching for events by name: " + keyword,
+      searchedEvents: events,
+    });
+  }
+}
